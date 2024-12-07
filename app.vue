cf22c0225ab5f7f7dc3517aad7434a119e82c3da
Ravinderpal sharma <138967300+rishv97790@users.noreply.github.com>
2024-12-07 00:41:41 -0500

    completed Frontend Book-review-site

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
